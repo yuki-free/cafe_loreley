621da03710552d1dd8b9c2a3322d7f58c409160b
maeharaFree <m.yuki.free@gmail.com>
2022-05-19 15:02:28 +0900

    フォーマット作成

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+import "./style.scss";
+
+console.log('hello');
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+// [定数] webpack の出力オプションを指定します
+// 'production' か 'development' を指定
+const MODE = "development";
+
+// ソースマップの利用有無(productionのときはソースマップを利用しない)
+const enabledSourceMap = MODE === "development";
+
+module.exports = {
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: `./src/index.js`,
+
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: "main.js"
+  },
+
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: MODE,
+
+  devServer: {
+    static: "dist",
+    open: true
+  },
+
+  module: {
+    rules: [
+      // Sassファイルの読み込みとコンパイル
+      {
+        test: /\.scss/, // 対象となるファイルの拡張子
+        use: [
+          // linkタグに出力する機能
+          "style-loader",
+          // CSSをバンドルするための機能
+          {
+            loader: "css-loader",
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込みを禁止する
+              url: false,
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap
+            },
+          },
+        ],
+      },
+    ],
+  },
+  // ES5(IE11等)向けの指定（webpack 5以上で必要）
+  target: ["web", "es5"],
+};
